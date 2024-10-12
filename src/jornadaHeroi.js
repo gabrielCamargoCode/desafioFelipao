f4d2a9dc0051275697fef91a14355facaf8bfebe
Gabriel Camargo dos Santos <105324565+gabrielCamargoCode@users.noreply.github.com>
2024-10-12 15:51:36 -0300

    Add files via upload

---
+++ src/jornadaHeroi.js
@@ -0,0 +1,34 @@
+//CÓDIGO COMPLETO
+
+let nomeHeroi = "Leonardo"
+let nivelExp = 200
+
+	console.log("Olá " + nomeHeroi + "!")
+    console.log("Seu nível de experiência é: " + nivelExp)
+
+
+    
+if(nivelExp <=1000){
+	console.log("O Heroi de nome " + nomeHeroi + " Está no nível Ferro.")
+}
+else if(nivelExp <=2000){
+	console.log("O Heroi de nome " + nomeHeroi + " Está no nível Bronze.")
+}
+else if(nivelExp <=5000){
+	console.log("O Heroi de nome " + nomeHeroi + " Está no nível Prata.")
+}
+else if(nivelExp <=7000){
+	console.log("O Heroi de nome " + nomeHeroi + " Está no nível Ouro.")
+}
+else if(nivelExp <=8000){
+	console.log("O Heroi de nome " + nomeHeroi + " Está no nível Platina.")
+}
+else if(nivelExp <=9000){
+	console.log("O Heroi de nome " + nomeHeroi + " Está no nível Ascendente.")
+}
+else if(nivelExp <=10000){
+	console.log("O Heroi de nome " + nomeHeroi + " Está no nível Imortal.")
+}
+else if(nivelExp >10000){
+	console.log("O Heroi de nome " + nomeHeroi + " Está no nível Radiante.")
+}
