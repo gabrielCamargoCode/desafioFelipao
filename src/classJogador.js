318fc962af5938785868ac21d61c1ed3774e5236
camargo <gabriel_camargo_dos_santos@hotmail.com>
2024-10-20 16:50:27 -0300

    Sua mensagem de commit aqui

--- src/classJogador.js
+++ src/classJogador.js
@@ -1,4 +1,4 @@
-class Heroi {
+class heroi {
     constructor(nome, idade, tipo) {
         this.nome = nome;
         this.idade = idade;
@@ -31,14 +31,14 @@ class Heroi {
 }
 
 // Exemplo de uso
-const heroi1 = new Heroi("Gandalf", 200, "mago");
+const heroi1 = new heroi("Gandalf", 200, "mago");
 heroi1.atacar();
 
-const heroi2 = new Heroi("Conan", 30, "guerreiro");
+const heroi2 = new heroi("Conan", 30, "guerreiro");
 heroi2.atacar();
 
-const heroi3 = new Heroi("Li", 25, "monge");
+const heroi3 = new heroi("Li", 25, "monge");
 heroi3.atacar();
 
-const heroi4 = new Heroi("Shadow", 22, "ninja");
+const heroi4 = new heroi("Shadow", 22, "ninja");
 heroi4.atacar();
